71e781402aae2c6fcc2fc984a70cd0aa6c98744a
Firdaus Alghifari <firdaus.alghifari@gmail.com>
2021-06-24 00:37:09 +0700

    update manifest config

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,8 +30,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'asdf-technology',
+        short_name: 'asdf-tech',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
